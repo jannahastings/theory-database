8641071b62f8168c0b41e361210a59fdb8030d93
Tom <tomjuggler@gmail.com>
2021-10-07 10:53:07 +0200

    quick test of new get_theory_visualisation(theory_list): function gets a list of theory numbers from front end and generates images for only these numbers saving to /test for now
    Signed-off-by: Tom <tomjuggler@gmail.com>

--- main.py
+++ main.py
@@ -17,7 +17,13 @@ import json
 from config import *
 from flask import Flask, render_template,request,url_for, redirect, session
 import TheoryDatabase
-from TheoryDatabase import Theory
+from TheoryDatabase import Theory, theories, Relation
+
+import networkx as nx
+import matplotlib
+# matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
 
 
 class FlaskApp(Flask):
@@ -36,6 +42,52 @@ class FlaskApp(Flask):
 app = FlaskApp(__name__)
 
 app.secret_key=SECRET_KEY
+
+
+def wrap_if_needed(string_val):
+    if ":" in string_val:
+        return( f'"{string_val}"')
+    return(string_val)
+
+def get_theory_visualisation(theory_list):
+    for theory_num in theories.keys():
+        if theory_num in theory_list:
+            theory = theories[theory_num]
+            G=nx.DiGraph()
+
+            for triple in theory.triples:
+                if triple.reified_rel is None:
+                    G.add_node(wrap_if_needed(triple.const1.name))
+                    G.add_node(wrap_if_needed(triple.const2.name))
+                    G.add_edge(wrap_if_needed(triple.const1.name),wrap_if_needed(triple.const2.name),label=triple.relStr)
+                else:
+                    G.add_node(wrap_if_needed(triple.const1.name))
+                    G.add_node(wrap_if_needed(triple.const2.name))
+                    G.add_node(triple.reified_rel.name,label=triple.relStr)
+                    G.add_edge(wrap_if_needed(triple.const1.name),triple.reified_rel.name,label=Relation.getStringForRelType(Relation.THROUGH))
+                    G.add_edge(triple.reified_rel.name,wrap_if_needed(triple.const2.name),label=Relation.getStringForRelType(Relation.TO))
+
+            pdot = nx.drawing.nx_pydot.to_pydot(G)
+
+            for i, node in enumerate(pdot.get_nodes()):
+                node_name = str(node).replace("\"","").replace(";","")
+                if node_name in theory.constructs_by_name.keys():
+                    node.set_shape('box')
+                    node.set_fontcolor('black')
+                    node.set_fillcolor('white')
+                    node.set_style('rounded, filled')
+                    node.set_color('black')
+                else:
+                    node.set_shape('ellipse')
+                    node.set_fontcolor('black')
+                    node.set_fontname('times italic')
+                    node.set_fillcolor('white')
+                    node.set_style('rounded, filled')
+                    node.set_color('grey')
+
+            png_path = "test/"+theory_num+".png"
+            pdot.write_png(png_path)
+    return pdot
 
 @app.route('/')
 def display_home():
@@ -113,6 +165,7 @@ def searchRelationResult(string=None):
 def show_theory_consistency():
     if request.method == 'POST':
         theories = request.form.get('theories')
+        get_theory_visualisation(theories)
         # print("GOT THEORIES for consistency: ",theories)
         session['theories'] = theories
         return redirect('/theoryConsistency')
@@ -122,6 +175,7 @@ def show_theory_consistency():
 def show_merged_theories():
     if request.method == 'POST':
         theories = request.form.get('theories')
+        get_theory_visualisation(theories)
         # print("GOT THEORIES for merged: ",theories)
         session['theories'] = theories
         return redirect('/mergedTheories')
